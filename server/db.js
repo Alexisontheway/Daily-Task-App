cc9faabe241a2fada82eef85942b88d00107820b
Alexisontheway <priyanshualex@gmail.com>
2026-03-25 01:13:22 +0530

    SSL fix

--- server/db.js
+++ server/db.js
@@ -1,15 +1,6 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
     ssl: {
         rejectUnauthorized: false
     }
 });
-
-module.exports = pool;
